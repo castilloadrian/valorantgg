11a40f0cfcbe6c7ad50b18e0253780e330788278
castilloadrian <adriancastillo1222@gmail.com>
2021-02-04 13:43:09 -0600

    react/express boilerplate

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+const axios = require('axios');
+
+const hitBackend = () => {
+  axios.get('http://localhost:8080/test')
+  .then((response) => {
+  console.log(response.data)
+  })
+  }
+
+function App() {
+  return (
+    <div className="App">
+      <button onClick={hitBackend}>Send request</button>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
