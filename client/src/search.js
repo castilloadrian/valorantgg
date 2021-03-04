537e8974b558dd60cc44ee1f0b27f4277ff97b9c
Matthew Merritt <matthewmerritt14@gamil.com>
2021-03-04 13:55:40 -0600

    Fixed images and console logging

--- client/src/search.js
+++ client/src/search.js
@@ -5,26 +5,40 @@ const axios = require('axios');
 const hitLeaderboard = () => {
     axios.get('http://localhost:8080/leaderboard')
     .then((response) => {
-    console.log(response.data.players);
+    var i = 0;
+    for(i;i<100;i++){
+        
+        console.log('Rank: ' + (i+1) + ' ' + response.data.players[i].gameName);
+    }
     })
 }
 
 const hitCharacters = () => {
     axios.get('http://localhost:8080/valcontent')
     .then((response) => {
-    console.log(response.data.characters)
+        var i = 0;
+        for(i;i<16;i++){
+            console.log(response.data.characters[i].name)
+        }
     })
 }
 const hitMaps = () => {
     axios.get('http://localhost:8080/valcontent')
     .then((response) => {
-    console.log(response.data.maps)
+        var i = 1;
+        for(i;i<7;i++){
+            console.log(response.data.maps[i].name)
+        }
     })
 }
 const hitSkins = () => {
     axios.get('http://localhost:8080/valcontent')
     .then((response) => {
-    console.log(response.data.skins)
+        var i = 0;
+        for(i;i<255;i++){
+            console.log(response.data.skins[i].name)
+        }
+    
     })
 }
 
@@ -32,7 +46,7 @@ const SearchBar = () => (
     <html>
         <header>
             <span>
-                <button type="submit" onClick={hitLeaderboard}>Top 500 Leaderboard</button>
+                <button type="submit" onClick={hitLeaderboard}>Top 100 Leaderboard</button>
                 <button type="submit" onClick={hitCharacters}>Character List</button>
                 <button type="submit" onClick={hitMaps}>Map List</button>
                 <button type="submit" onClick={hitSkins}>All Weapon Skins</button>
@@ -46,7 +60,7 @@ const SearchBar = () => (
         </header>
         <body>
             <span>
-                    <img src="/client/public/jett.jpg" alt="jett cant be displayed" align="left"/>
+                    <img src="https://d.newsweek.com/en/full/1578649/valorant-jett-abilities-kit-release.jpg?w=1600&h=1600&l=46&t=9&q=88&f=612b4c14adfe75d49162b7fa83cc3bfb" width="400" height="500" alt="jett cant be displayed" align="left"/>
             </span>
             <span>
                     <form action="/" method="get" id="form"> 
@@ -60,7 +74,7 @@ const SearchBar = () => (
                     </form>
             </span>
             <span>
-                    <img src="./../public/pheonix.jpg" alt="pheonix cant be displayed" align="right"/>
+                    <img src="https://i.ytimg.com/vi/ttJMFW2wUQM/maxresdefault.jpg" width="400" height="500" alt="pheonix cant be displayed" align="right"/>
             </span>
         </body>
     </html>
