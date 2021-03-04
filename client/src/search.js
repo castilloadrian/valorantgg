07b03453c452c6a329c3589af91cce4a062d5836
castilloadrian <adriancastillo1222@gmail.com>
2021-03-04 00:15:38 -0600

    some frontend done. more to come

--- client/src/search.js
+++ client/src/search.js
@@ -1,16 +1,33 @@
+import "./search.css";
+
 const SearchBar = () => (
-    <form action="/" method="get">
-        <label htmlFor="header-search">
-            <span className="visually-hidden">Search Players: </span>
-        </label>
-        <input
-            type="text"
-            id="header-search"
-            placeholder="Find an Agent, ie. player#NA1"
-            name="s" 
-        />
-        <button type="submit">Search</button>
-    </form>
+    <html>
+        <header>
+            <h1 align="center">
+                VALORANTGG
+            </h1>
+            <hr></hr><br></br>
+        </header>
+        <body>
+            <span>
+                    <img src="/client/public/jett.png" alt="jett cant be displayed" align="left"/>
+            </span>
+            <span>
+                    <form action="/" method="get" id="form"> 
+                        <input
+                            type="text"
+                            id="header-search"
+                            placeholder="Find an Agent, ie. player#NA1"
+                            name="s" 
+                        />
+                        <button type="submit">Search</button>
+                    </form>
+            </span>
+            <span>
+                    <img src="./../public/pheonix.png" alt="pheonix cant be displayed" align="right"/>
+            </span>
+        </body>
+    </html>
 );
 
 export default SearchBar;
