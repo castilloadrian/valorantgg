dc13669800188493181930a54829954f82356ff7
Matthew Merritt <matthewmerritt14@gamil.com>
2021-04-01 00:40:46 -0500

    Maps finished w/ updated api key for sprint 3

--- server/routes/leaderboard.js
+++ server/routes/leaderboard.js
@@ -7,7 +7,7 @@ router.get('/leaderboard', function(req, res, next) {
   request({
     uri: 'https://na.api.riotgames.com/val/ranked/v1/leaderboards/by-act/ab57ef51-4e59-da91-cc8d-51a5a2b9b8ff?startIndex=0',
     qs: {
-      api_key: 'RGAPI-ce6e6ad1-86a5-42ec-b3e7-51e0b5e0de99',
+      api_key: 'RGAPI-67953cdc-fe92-48a0-a9bd-182ecedde455',
       query: '52e9749a-429b-7060-99fe-4595426a0cf7'
     }
   }).pipe(res);
--- server/routes/valcontent.js
+++ server/routes/valcontent.js
@@ -7,7 +7,7 @@ router.get('/valcontent', function(req, res, next) {
   request({
     uri: 'https://na.api.riotgames.com/val/content/v1/contents',
     qs: {
-      api_key: 'RGAPI-ce6e6ad1-86a5-42ec-b3e7-51e0b5e0de99',
+      api_key: 'RGAPI-67953cdc-fe92-48a0-a9bd-182ecedde455',
       query: '52e9749a-429b-7060-99fe-4595426a0cf7'
     }
   }).pipe(res);
