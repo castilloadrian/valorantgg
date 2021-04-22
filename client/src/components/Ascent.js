e3945fbb56fce83f363c1fc38514daa919b76f71
castilloadrian <adriancastillo1222@gmail.com>
2021-04-21 20:01:53 -0500

    ascent sample page

--- client/src/components/Ascent.js
+++ client/src/components/Ascent.js
@@ -3,7 +3,60 @@ import './../styles/Ascent.css';
 function AscentScreen() {
    return (
     <div class="whole7">
-        <h1>Ascent</h1>
+        ASCENT
+        <br></br><hr></hr><br></br>
+        <div>
+            <img src="https://cdn1.dotesports.com/wp-content/uploads/2020/06/03123207/ascent.png" class="ascentmap"></img>
+        </div>
+        <br></br>
+        <table class="table">
+            <tr>
+                <th>Map Callouts:</th>
+                <th>Best Agents:</th>
+                <th>Winrates:</th>
+            </tr>
+            <tr>
+                <td>
+                    <ul class="list">
+                        <li>A site</li>
+                        <li>B site</li>
+                        <li>Boat House</li>
+                        <li>Bottom</li>
+                        <li>Lane / Cat walk</li>
+                        <li>Button</li>
+                        <li>B Main</li>
+                        <li>Tiles</li>
+                        <li>Market</li>
+                        <li>Pizza</li>
+                        <li>Arches</li>
+                        <li>Top Mid</li>
+                        <li>Bottom Mid</li>
+                        <li>Gelato</li>
+                        <li>Garden / Tree</li>
+                        <li>Window</li>
+                        <li>A Main</li>
+                        <li>Wine</li>
+                        <li>Heaven</li>
+                        <li>CT Spawn</li>
+                        <li>T Spawn</li>
+                        <li>Mid</li>
+                    </ul>
+                </td>
+                <td>
+                    <ul class="list">
+                        <li>Sova</li>
+                        <li>Omen</li>
+                        <li>Jett</li>
+                    </ul>
+                </td>
+                <td>
+                    <ul class="list">
+                        <li>Attacking: 47.2%</li>
+                        <li>Defending: 52.8%</li>
+                    </ul>
+                </td>
+            </tr>
+        </table>
     </div>
    );
 }
