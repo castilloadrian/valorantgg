353e7eaf0834a5b11b64c26ae74aabe37f9d68ef
castilloadrian <adriancastillo1222@gmail.com>
2021-04-30 12:47:24 -0500

    riot route for approval

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,6 +10,7 @@ import Haven from './Haven'
 import Icebox from './Icebox'
 import Bind from './Bind'
 import Split from './Split'
+import Riot from './Riot'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 
@@ -28,8 +29,7 @@ function App() {
         <Route path="/Maps/Icebox" component={Icebox}/>
         <Route path="/Maps/Bind" component={Bind}/>
         <Route path="/Maps/Split" component={Split}/>
-
-
+        <Route path="/Riot" component={Riot}/>
       </Switch>
     </Router>
   );
---
+++ client/src/components/Riot.js
@@ -0,0 +1,11 @@
+import './../styles/Riot.css';
+
+const RiotScreen = () => (
+    <div class="whole15">
+        <br></br>
+        <h1>ec694a3998bb</h1>
+    </div>
+);
+
+
+export default RiotScreen;
